dd93755e66b5df4c0668826658cc056f78f04e06
tuando90 <tuan.do@3i.ai>
2024-03-10 17:33:40 +0900

    Add GPS BLE program to send GPS over BLE

---
+++ examples/GPS_BLE/GPS_BLE.ino
@@ -0,0 +1,278 @@
+/**
+ * @file      MinimalModemGPSExample.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2022  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2022-09-16
+ *
+ */
+#include <Arduino.h>
+#define XPOWERS_CHIP_AXP2101
+#include "XPowersLib.h"
+#include "utilities.h"
+
+XPowersPMU  PMU;
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
+
+#define TINY_GSM_RX_BUFFER 1024
+
+#define TINY_GSM_MODEM_SIM7080
+#include <TinyGsmClient.h>
+#include "utilities.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(Serial1, Serial);
+TinyGsm        modem(debugger);
+#else
+TinyGsm        modem(SerialAT);
+#endif
+
+// MARK: Bluetooth
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLEDescriptor *pDescr;
+BLE2902 *pBLE2902;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+// MARK: GPS
+float lat2      = 0;
+float lon2      = 0;
+float speed2    = 0;
+float alt2      = 0;
+int   vsat2     = 0;
+int   usat2     = 0;
+float accuracy2 = 0;
+int   year2     = 0;
+int   month2    = 0;
+int   day2      = 0;
+int   hour2     = 0;
+int   min2      = 0;
+int   sec2      = 0;
+bool  level     = false;
+
+void setup()
+{
+
+    Serial.begin(115200);
+
+    //Start while waiting for Serial monitoring
+    while (!Serial);
+
+    delay(3000);
+
+    Serial.println();
+
+    // Setup BLE
+    setupBLE();
+
+    /*********************************
+     *  step 1 : Initialize power chip,
+     *  turn on modem and gps antenna power channel
+    ***********************************/
+    if (!PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL)) {
+        Serial.println("Failed to initialize power.....");
+        while (1) {
+            delay(5000);
+        }
+    }
+    //Set the working voltage of the modem, please do not modify the parameters
+    PMU.setDC3Voltage(3000);    //SIM7080 Modem main power channel 2700~ 3400V
+    PMU.enableDC3();
+
+    //Modem GPS Power channel
+    PMU.setBLDO2Voltage(3300);
+    PMU.enableBLDO2();      //The antenna power must be turned on to use the GPS function
+
+    // TS Pin detection must be disable, otherwise it cannot be charged
+    PMU.disableTSPinMeasure();
+
+
+    /*********************************
+     * step 2 : start modem
+    ***********************************/
+
+    Serial1.begin(115200, SERIAL_8N1, BOARD_MODEM_RXD_PIN, BOARD_MODEM_TXD_PIN);
+
+    pinMode(BOARD_MODEM_PWR_PIN, OUTPUT);
+
+    digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+    delay(100);
+    digitalWrite(BOARD_MODEM_PWR_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+
+    int retry = 0;
+    while (!modem.testAT(1000)) {
+        Serial.print(".");
+        if (retry++ > 15) {
+            // Pull down PWRKEY for more than 1 second according to manual requirements
+            digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+            delay(100);
+            digitalWrite(BOARD_MODEM_PWR_PIN, HIGH);
+            delay(1000);
+            digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+            retry = 0;
+            Serial.println("Retry start modem .");
+        }
+    }
+    Serial.println();
+    Serial.print("Modem started!");
+
+    /*********************************
+     * step 3 : start modem gps function
+    ***********************************/
+
+    //  When configuring GNSS, you need to stop GPS first
+    modem.disableGPS();
+    delay(500);
+
+#if 0
+    /*
+    ! GNSS Work Mode Set
+     <gps mode> GPS work mode.
+        1 Start GPS NMEA out.
+    <glo mode> GLONASS work mode.
+        0 Stop GLONASS NMEA out.
+        1 Start GLONASS NMEA out.
+    <bd mode> BEIDOU work mode.
+        0 Stop BEIDOU NMEA out.
+        1 Start BEIDOU NMEA out.
+    <gal mode> GALILEAN work mode.
+        0 Stop GALILEAN NMEA out.
+        1 Start GALILEAN NMEA out.
+    <qzss mode> QZSS work mode.
+        0 Stop QZSS NMEA out.
+        1 Start QZSS NMEA out.*/
+
+    //GNSS Work Mode Set GPS+BEIDOU
+    modem.sendAT("+CGNSMOD=1,1,0,0,0");
+    modem.waitResponse();
+
+
+    /*
+    GNSS Command,For more parameters, see <SIM7070_SIM7080_SIM7090 Series_AT Command Manual> 212 page.
+    <minInterval> range: 1000-60000 ms
+     minInterval is the minimum time interval in milliseconds that must elapse between position reports. default value is 1000.
+    <minDistance> range: 0-1000
+     Minimum distance in meters that must be traversed between position reports. Setting this interval to 0 will be a pure time-based tracking/batching.
+    <accuracy>:
+        0  Accuracy is not specified, use default.
+        1  Low Accuracy for location is acceptable.
+        2 Medium Accuracy for location is acceptable.
+        3 Only High Accuracy for location is acceptable.
+    */
+    // minInterval = 1000,minDistance = 0,accuracy = 0
+    modem.sendAT("+SGNSCMD=2,1000,0,0");
+    modem.waitResponse();
+
+    // Turn off GNSS.
+    modem.sendAT("+SGNSCMD=0");
+    modem.waitResponse();
+#endif
+    delay(500);
+
+    // GPS function needs to be enabled for the first use
+    if (modem.enableGPS() == false) {
+        Serial.print("Modem enable gps function failed!!");
+        while (1) {
+            delay(5000);
+        }
+    }
+
+}
+
+void setupBLE() {
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );                   
+
+  // Create a BLE Descriptor
+  
+  pDescr = new BLEDescriptor((uint16_t)0x2901);
+  pDescr->setValue("A very interesting variable");
+  pCharacteristic->addDescriptor(pDescr);
+  
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  pCharacteristic->addDescriptor(pBLE2902);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop()
+{
+    if (modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
+                     &year2, &month2, &day2, &hour2, &min2, &sec2)) {
+        Serial.println();
+        Serial.print("lat:"); Serial.print(String(lat2, 8)); Serial.print("\t");
+        Serial.print("lon:"); Serial.print(String(lon2, 8)); Serial.println();
+        Serial.print("speed:"); Serial.print(speed2); Serial.print("\t");
+        Serial.print("altitude:"); Serial.print(alt2); Serial.println();
+        Serial.print("year:"); Serial.print(year2);
+        Serial.print(" montoh:"); Serial.print(month2);
+        Serial.print(" day:"); Serial.print(day2);
+        Serial.print(" hour:"); Serial.print(hour2);
+        Serial.print(" minutes:"); Serial.print(min2);
+        Serial.print(" second:"); Serial.print(sec2); Serial.println();
+        Serial.println();
+
+        // Convert latitude and longitude to string
+        String coordinates = String(lat2, 8) + "," + String(lon2, 8);
+        // Send coordinates over BLE
+        pCharacteristic->setValue(coordinates.c_str());
+        pCharacteristic->notify();
+
+        // After successful positioning, the PMU charging indicator flashes quickly
+        PMU.setChargingLedMode(XPOWERS_CHG_LED_BLINK_4HZ);
+        delay(1000);
+    } else {
+        // Blinking PMU charging indicator
+        PMU.setChargingLedMode(level ? XPOWERS_CHG_LED_ON : XPOWERS_CHG_LED_OFF);
+        level ^= 1;
+        delay(1000);
+    }
+}
